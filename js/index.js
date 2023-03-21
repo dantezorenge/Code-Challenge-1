a67ee86c3ca6dbb9853d5b11fa49c1fb383c2b2e
dantezorenge <orenged524@gmail.com>
2023-03-21 10:10:15 +0300

    add changes

---
+++ js/index.js
@@ -0,0 +1,40 @@
+function Convert() {
+let number=parseInt(grade.value);
+if (number=>101){
+    grade="Please input valid marks";}
+    else if(number=>80){
+        grade="Congratulations your grade is A"
+    }
+    else if(number=>60){
+        grade="your grade is B";}
+    else if(number=>49){
+        grade="your grade is C"}
+    else if(number=>40){
+        grade="your grade is D"}
+    else if(number<40, number>0){
+        grade="Your grade is E"}
+    else{
+        grade="Kindly input correct marks"
+    }
+//the return value
+console.log(grade)
+gradeShow.innerText='${grade}'}
+
+
+
+speed = int(input("Enter the speed of the car: "))
+
+if (speed < 70)
+   { print("Ok")}
+else
+    demerit_points = (speed - 70) // 5
+    if (demerit_points > 12)
+        {print("License suspended")}
+    else
+    {print("Points:", demerit_points)}
+
+function netSalary() {
+    
+}
+
+
